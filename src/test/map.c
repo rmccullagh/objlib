e6006d5046c260bbcf6c23391da0b8bcf46ef623
ryan <ryan@debian>
2017-11-22 12:08:15 -0600

    add a test to ensure collisions of the djb2 hashing algo

--- src/test/map.c
+++ src/test/map.c
@@ -6,8 +6,63 @@
 /* For freeing */
 static como_object *root = NULL;
 
+typedef struct _spair {
+  char *s1;
+  char *s2;
+} spair;
+
+static spair collisions[] = {
+  { "hetairas",    "mentioner"  },
+  { "heliotropes", "neurospora" },
+  { "depravement", "serafins"   },
+  { "stylist",     "subgenera"  },
+  { "joyful",      "synaphea"   },
+  { "redescribed", "urites"     },
+  { "dram",        "vivency"    },
+  { NULL,          NULL         }
+};
+
+static void test_collisions(void)
+{
+  como_object *root = NULL;
+
+  /* Make sure to not test the resize function */
+  como_object *map = como_map_new(16);
+  map->next = root;
+  root = map;
+
+  como_size_t i;
+
+  for(i = 0; collisions[i].s1 != NULL; i++)
+  {
+    spair pair = collisions[i];
+    
+    como_object *s1 = como_stringfromstring(pair.s1);
+    s1->next = root;
+    root = s1;
+
+    como_object *s2 = como_stringfromstring(pair.s2);
+    s2->next = root;
+    root =s2;
+
+    como_map_put(map, s1, s1);
+    como_map_put(map, s2, s2);
+  }
+
+  while(root != NULL)
+  {
+    como_object *next = root->next;
+    
+    como_object_dtor(root);
+
+    root = next;
+  }
+}
+
 int main(void)
 {
+  test_collisions();
+
   como_object *map = como_map_new(8);
   como_size_t i;
 
